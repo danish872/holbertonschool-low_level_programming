ca0f378585f6aa1d4b7bd15a2b00810f812bf55b
danish872 <9745@holbertonstudents.com>
2024-10-25 15:06:23 +0200

    3-islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - Check Holberton
- * @c: An input character
+ * _islower - main
+ * c: A character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
