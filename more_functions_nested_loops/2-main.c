29aa1a65ccd86b6643107473cc150ec4b51ee4b9
danish872 <9745@holbertonstudents.com>
2024-10-28 14:43:46 +0100

    2main

---
+++ more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  printf("%d\n", mul(10, 200));
+  printf("%d\n", mul(-402, 40));
+  return (0);
+}
