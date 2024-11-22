57c10856fcf27b45b3140a82d7f67f1875a79823
danish872 <danishimwe872@gmail.com>
2024-11-22 10:39:05 +0000

    1-array-iterator

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef main_h
 #define main_h
+#include <stddef.h>
 /*
  * prototype
  *
  */ 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
