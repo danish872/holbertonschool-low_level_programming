86d8df051870256df92068654914f759478b0d4c
danish872 <danishimwe872@gmail.com>
2025-01-07 18:14:18 +0100

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- *  * malloc_checked - allocate memory using malloc.
- *   * Description: If malloc fails, terminate process with status 98.
- *    * @b: unsigned int memory size to allocate
- *     * Return: void pointer to malloc'd memory space
- *      */
+ *malloc_checked - allocate memory using malloc.
+ *Description: If malloc fails, terminate process with status 98.
+ *@b: unsigned int memory size to allocate
+ *Return: void pointer to malloc'd memory space
+ */
 
 void *malloc_checked(unsigned int b)
 {
