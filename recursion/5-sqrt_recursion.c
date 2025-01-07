e67934c2d376d28aba2b49127487fe6aacf9ae40
danish872 <danishimwe872@gmail.com>
2025-01-07 17:21:13 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,16 +1,36 @@
 #include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+
 /**
-* _pow_recursion - function that returns the
-* value of x raised to the power of y
-*@x: the base
-*@y: the exponent
-*Return: the value of x raised to the power of y
-*/
-int _pow_recursion(int x, int y)
+  * _sqrt_recursion - Returns the natural square root of a number
+  * @n: number to calculate the natural square root
+  *
+  * Return: the natural square root
+  */
+int _sqrt_recursion(int n)
 {
-		if (y < 0)
-			return (-1);
-		else if (y == 0)
-			return (1);
-		return (_pow_recursion(x, y - 1) * x);
+	return (_sqrt(n, 1));
 }
+
+/**
+  * _sqrt - Calculates natural square root
+  * @n: number to calculate the square root
+  * @i: iterate number
+  *
+  * Return: the natural square root
+  */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
+
