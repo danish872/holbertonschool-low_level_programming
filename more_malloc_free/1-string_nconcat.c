ef8cae0e14f64d21241682fbb1d80b67d4b56a7e
danish872 <danishimwe872@gmail.com>
2025-01-07 18:14:57 +0100

    correction moremalloc 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *  * string_nconcat - concatenate 2 strings, only n bytes of s2
- *   * @s1: string 1
- *    * @s2: string 2
- *     * @n: bytes to include of s2
- *      * Return: NULL if fail, else pointer to malloc memory
- *       */
+ *string_nconcat - concatenate 2 strings, only n bytes of s2
+ *@s1: string 1
+ *@s2: string 2
+ *@n: bytes to include of s2
+ *Return: NULL if fail, else pointer to malloc memory
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
