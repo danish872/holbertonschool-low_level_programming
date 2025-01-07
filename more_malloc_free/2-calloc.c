7f92ec2c5459a910a6712818946badf4142ba53c
danish872 <danishimwe872@gmail.com>
2025-01-07 18:15:14 +0100

    correction moremalloc 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- *  * _calloc - allocates memory for an array given number of elements and size
- *   * @nmemb: number of elements
- *    * @size: size of each element
- *     * Return: Null if error, else pointer to allocated memory
- *      */
+ *_calloc - allocates memory for an array given number of elements and size
+ *@nmemb: number of elements
+ *@size: size of each element
+ *Return: Null if error, else pointer to allocated memory
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
