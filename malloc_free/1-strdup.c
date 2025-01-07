252ab4f067deffc8e7b9016d760c44bedc6ed35c
danish872 <danishimwe872@gmail.com>
2025-01-07 18:03:42 +0100

    correction malloc 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- *  * _strdup - Duplicate a string using malloc
- *   * @str: string to duplicate
- *    * Return: Pointer to a the new duped string
- *     */
+ *_strdup - Duplicate a string using malloc
+ *@str: string to duplicate
+ *Return: Pointer to a the new duped string
+ */
 
 char *_strdup(char *str)
 {
