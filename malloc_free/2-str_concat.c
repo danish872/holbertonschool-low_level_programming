35c1800e8ae4b66c15137277cc2f9f0cef27b00b
danish872 <danishimwe872@gmail.com>
2025-01-07 18:04:00 +0100

    correction malloc 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- *  * str_concat - concatenate two strings using malloc
- *   * @s1: string 1
- *    * @s2: string 2
- *     * Return: pointer to concat string
- *      */
+ *str_concat - concatenate two strings using malloc
+ *@s1: string 1
+ *@s2: string 2
+ *Return: pointer to concat string
+ */
 
 char *str_concat(char *s1, char *s2)
 {
