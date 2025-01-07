1d818935f27207fb386cdf98e4d70cd245a08e90
danish872 <danishimwe872@gmail.com>
2025-01-07 18:09:58 +0100

    correction more malloc 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,28 +11,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-		char *p;
-			int strlen1, i, c;
+	char *p;
+	int strlen1, i, c;
 
-				if (s1 == NULL)
-							s1 = "";
-					if (s2 == NULL)
-								s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-						strlen1 = (unsigned int)_strlen(s1);
-							p = malloc((strlen1 + n + 1) * sizeof(char));
-								if (p == NULL)
-											return (NULL);
-									for (i = 0, c = 0; i < (strlen1 + n); i++)
-											{
-														if (i < strlen1)
-																		p[i] = s1[i];
-																else
-																				p[i] = s2[c++];
-																	}
-										p[i] = '\0';
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	{
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
+	}
+	p[i] = '\0';
 
-											return (p);
+	return (p);
 }
 
 /**
@@ -43,9 +43,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 int _strlen(char *s)
 {
-		int i;
+	int i;
 
-			for (i = 0; s[i] != '\0'; i++)
-						;
-				return (i);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
