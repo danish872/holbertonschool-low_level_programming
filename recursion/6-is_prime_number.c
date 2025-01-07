d650281623c25ec82d9c1415d64f2e4a32835f9f
danish872 <danishimwe872@gmail.com>
2025-01-07 17:23:33 +0100

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,16 +1,37 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
+
+int check_prime(int n, int i);
+
 /**
-* _pow_recursion - function that returns the
-* value of x raised to the power of y
-*@x: the base
-*@y: the exponent
-*Return: the value of x raised to the power of y
-*/
-int _pow_recursion(int x, int y)
+  * is_prime_number - Returns if a number is prime
+  * @n: the number to be checked
+  *
+  * Return: integer value
+  */
+int is_prime_number(int n)
 {
-		if (y < 0)
-			return (-1);
-		else if (y == 0)
-			return (1);
-		return (_pow_recursion(x, y - 1) * x);
+	return (check_prime(n, 1));
 }
+
+/**
+  * check_prime - Check if number is prime
+  * @n: the number to be checked
+  * @i: the iteration times
+  *
+  * Return: 1 for prime or 0 composite
+  */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
+		return (1);
+
+	return (check_prime(n, i + 1));
+}
+
