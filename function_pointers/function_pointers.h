0812e657d6a616a149f3f98068a1f7b31e393700
danish872 <danishimwe872@gmail.com>
2025-03-18 14:12:54 +0100

    commit of function

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,9 +2,9 @@
 #define main_h
 #include <stddef.h>
 /*
- * prototype
+ *prototype
  *
- */ 
+ */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
