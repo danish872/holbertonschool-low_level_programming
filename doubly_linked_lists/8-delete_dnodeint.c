773d63b4f359bfde46bf26b879c2d2d4f66af513
danish872 <danishimwe872@gmail.com>
2025-04-04 14:43:29 +0200

    commit of the corrected file 8-deleted_dnodeint

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,43 @@
 #include "lists.h"
 /**
- * delete_dnodeint_at_index - function
- * @head: double poitner to first node in lnkd list
- * @index: index to where to delete
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
+ * @head: Double pointer to head of the list.
+ * @index: Index of the node to delete.
  *
- * Description: function that deletes the node at nth of a lnkd list
- * Return: 1 Success, -1 Fail
+ * Return: 1 on success, -1 on failure.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp, *temp2;
-	unsigned int idx = 0;
+	dlistint_t *temp = *head;
+	unsigned int i;
 
-	if (*head == NULL)
-	{
+	if (!head || !*head)
 		return (-1);
-	}
+
+	/* Delete the head node */
 	if (index == 0)
 	{
-		temp = (*head)->next;
-		temp->prev = NULL;
-		/*free(temp);*/
+		*head = temp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(temp);
 		return (1);
 	}
-	temp = *head;
-	while (temp != NULL)
-	{
-		if (idx == index)
-		{
-			temp2 = temp->next;
-			temp2->prev = temp->prev->prev;
-			temp = temp2->prev;
-			temp->next = temp2->next->next;
-			/*free(temp2);*/
-			return (1);
-		}
-		temp = temp->next;
-		idx++;
-	}
-	if (idx < index)
-		return (-1);
-	if (temp->next == NULL)
-		return (1);
 
-	return (-1);
+	/* Traverse to the node at index */
+	for (i = 0; temp && i < index; i++)
+		temp = temp->next;
+
+	if (!temp)
+		return (-1);
+
+	/* Bypass the node */
+	if (temp->prev)
+		temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+
+	free(temp);
+	return (1);
 }
+
