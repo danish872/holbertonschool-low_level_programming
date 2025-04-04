cdf84d2cda42cf730a08717543147c99a5ee14a4
danish872 <danishimwe872@gmail.com>
2025-04-04 15:05:15 +0200

    commit of corrected file 0-readtextfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,37 +1,54 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include "main.h"
+
 /**
- * read_textfile - function
- * @filename: file to read
- * @letters: # of letters should read/print
+ * read_textfile - Reads and prints a text file to stdout
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
  *
- * Description: func to read and print to stdout
- * Return: # of letters actually read/print
+ * Return: Actual number of letters it could read and print, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
-	int fd, wr;
-	ssize_t sz;
+	int fd;
+	ssize_t sz, wr;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	buff = (char *)malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
+	{
+		close(fd);
 		return (0);
+	}
+
 	sz = read(fd, buff, letters);
 	if (sz == -1)
+	{
+		free(buff);
+		close(fd);
 		return (0);
+	}
 
-	buff[sz] = '\0';
 	wr = write(STDOUT_FILENO, buff, sz);
-	if (wr == -1)
+	if (wr == -1 || wr != sz)
+	{
+		free(buff);
+		close(fd);
 		return (0);
+	}
 
 	free(buff);
 	close(fd);
 	return (sz);
 }
+
